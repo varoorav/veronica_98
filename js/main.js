873ac18fd6a544ba7e426a3158138604a80cc8ef
varoorav <63037165+varoorav@users.noreply.github.com>
2020-12-09 18:13:09 +0900

    new

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var leftSectionlEl = document.getElementById("leftSection");
 // 선
 var verticalLine1El = document.getElementById("verticalLine1");
 var verticalLine2El = document.getElementById("verticalLine2");
-
+var mainImgEl = document.getElementById("mainImg");
 
 
 /*---*/
@@ -18,11 +18,13 @@ function OnMenuWhite(e) {
     leftSectionlEl.style.backgroundColor = "#ffffff";
     leftSectionlEl.style.color = "#000000";
     verticalLine1El.style.borderLeftColor = "#000000";
+    mainImgEl.src = "./img/normalLogo.png";
 }
 function OnMenuBlack(e) {
     leftSectionlEl.style.backgroundColor = "#000000";
     leftSectionlEl.style.color = "#ffffff";
     verticalLine1El.style.borderLeftColor = "#ffffff";
+    mainImgEl.src = "./img/greyLogo.png";
 }
 prolEl.addEventListener("mouseover", OnMenuBlack);
 prolEl.addEventListener("mouseout", OnMenuWhite);
@@ -33,6 +35,7 @@ epilEl.addEventListener("mouseout", OnMenuWhite);
 function OnMenuCyan(e) {
     leftSectionlEl.style.backgroundColor = "#00bdff";
     leftSectionlEl.style.color = "#000000";
+    mainImgEl.src = "./img/greyLogo.png";
 }
 illuEl.addEventListener("mouseover", OnMenuCyan);
 illuEl.addEventListener("mouseout", OnMenuWhite);
@@ -41,6 +44,7 @@ illuEl.addEventListener("mouseout", OnMenuWhite);
 function OnMenuMagenta(e) {
     leftSectionlEl.style.backgroundColor = "#ff0098";
     leftSectionlEl.style.color = "#000000";
+    mainImgEl.src = "./img/greyLogo.png";
 }
 newmEl.addEventListener("mouseover", OnMenuMagenta);
 newmEl.addEventListener("mouseout", OnMenuWhite);
@@ -49,6 +53,7 @@ newmEl.addEventListener("mouseout", OnMenuWhite);
 function OnMenuYellow(e) {
     leftSectionlEl.style.backgroundColor = "#ffc200";
     leftSectionlEl.style.color = "#000000";
+    mainImgEl.src = "./img/greyLogo.png";
 }
 videEl.addEventListener("mouseover", OnMenuYellow);
 videEl.addEventListener("mouseout", OnMenuWhite);
